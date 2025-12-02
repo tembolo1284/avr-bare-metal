c241deabd1c91e686a82aea4609dafe5ed8cddd8
Paul Nikholas Lopez <nik.lopez381@gmail.com>
2025-12-02 02:03:30 -0500

    Fixing Makefiles.

--- c-project/common.mk
+++ c-project/common.mk
@@ -14,17 +14,17 @@ CFLAGS   = -Os -DF_CPU=$(F_CPU) -mmcu=$(MCU) -Wall -Wextra -std=c99
 
 # Build rules (TARGET must be defined by including Makefile)
 %.o: %.c
-	$(CC) $(CFLAGS) -c -o $@ $
+	$(CC) $(CFLAGS) -c -o $@ $<
 
 %.elf: %.o
 	$(CC) $(CFLAGS) -o $@ $^
 
 %.hex: %.elf
 	$(OBJCOPY) -O ihex -R .eeprom $< $@
-	$(SIZE) --mcu=$(MCU) -C $
+	$(SIZE) --mcu=$(MCU) -C $<
 
 flash: $(TARGET).hex
-	sudo $(AVRDUDE) -F -V -c arduino -p $(MCU) -P $(PORT) -b $(BAUD) -U flash:w:$
+	sudo $(AVRDUDE) -F -V -c arduino -p $(MCU) -P $(PORT) -b $(BAUD) -U flash:w:$<
 
 clean:
 	rm -f *.o *.elf *.hex
